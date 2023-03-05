48c8f2cc8dda7bfa775a1a2460a1aad552eb2e25
Tanya Panther <tanya.panther@mail.ru>
2023-03-05 21:32:21 +0900

    add HW1 HW2 gitignore

---
+++ HW1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+
+Console.Write("Введите числа (через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = arr.Count(x => x > 0);
+Console.WriteLine($"Количество чисел > 0: {count}");
